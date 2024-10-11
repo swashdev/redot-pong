10737159f7617fde22292be27c1fa25a3fafe81e
swashberry <swashdev@pm.me>
2024-10-11 17:51:44 -0500

    Initialize the ball.
    This commit adds the `RedotPongBall` class, which is used to draw balls
    and determine the outer boundaries of them.

---
+++ balls/ball.gd
@@ -0,0 +1,73 @@
+class_name RedotPongBall
+extends Node2D
+# A ball to bounce back and forth.
+
+
+#region Variables
+
+# The radius of the ball.
+@export_range(1.0, 20.0, 1.0, "or_greater") var radius: float = 10.0
+
+# The ball's color.
+@export_color_no_alpha var _color: Color = Color.WHITE:
+	get = get_color, set = set_color
+
+
+#region Properties of the Circle
+
+var diameter: float:
+	get:
+		return radius * 2
+	set(value):
+		radius = value / 2
+
+# `top`, `bottom`, `left`, and `right` are derived from an approximation of
+# the ball as a non-rotating square, for collision detection purposes.
+
+var top: float:
+	get:
+		return position.y - radius
+	set(value):
+		position.y = value + radius
+
+var bottom: float:
+	get:
+		return position.y + radius
+	set(value):
+		position.y = value - radius
+
+var left: float:
+	get:
+		return position.x - radius
+	set(value):
+		position.x = value + radius
+
+var right: float:
+	get:
+		return position.x + radius
+	set(value):
+		position.x = value - radius
+
+#endregion Properties of the Circle
+
+#endregion Variables
+
+#region System Functions
+
+# Draws the ball
+func _draw() -> void:
+	draw_circle(position, radius, _color, true)
+
+#endregion System Functions
+
+#region Setters & Getters
+
+func get_color() -> Color:
+	return _color
+
+func set_color(value: Color) -> void:
+	_color.r = value.r
+	_color.g = value.g
+	_color.b = value.b
+
+#endregion Setters & Getters
