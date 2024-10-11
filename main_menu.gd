7e2c7fb8e1df5296a410fac31a5b69121bc318e9
swashberry <swashdev@pm.me>
2024-10-11 14:30:59 -0500

    Add the version number to the main menu.
    The main menu will display the version number in the corner upon
    initialization.  This message will also disclose what version of Redot
    it is being run on.

---
+++ main_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+# The main menu for Redot Pong.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Fill out the version number label with comprehensive version data.
+	var redot_pong_version: String = Global.get_version_string()
+	var redot_engine_version: String = Engine.get_version_info()["string"]
+	if OS.is_debug_build():
+		redot_engine_version += " (debug)"
+	var version_text: String = "Version " + redot_pong_version + ", " + \
+			"running on Redot Engine " + redot_engine_version
+
+	$VersionNumberLabel.set_text(version_text)
