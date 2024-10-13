975d861867f2d2041447c30d8c7c271360f9c141
swashberry <swashdev@pm.me>
2024-10-13 03:14:14 -0500

    Implement unpausing.
    Unpausing is handled by the function `_on_main_menu_requested_unpause`,
    which is also used by the `_on_main_menu_requested_new_game` function to
    reduce code redundancy.

--- main/main.gd
+++ main/main.gd
@@ -5,6 +5,7 @@ extends Node2D
 #region Child Nodes
 
 @onready var main_menu = $MainMenu
+@onready var resume_button = $MainMenu/Buttons/ResumeButton
 @onready var game = $Game
 
 #endregion Child Nodes
@@ -17,12 +18,22 @@ func _ready() -> void:
 
 # The "Start Game" button has been pressed, and it is time to start a new game.
 func _on_main_menu_requested_new_game(two_players: bool) -> void:
-	# Hide the main menu first.
-	main_menu.hide()
 	# Instruct `game` to set up for a new game.
 	await game.new_game(two_players)
 	# Unpause the game.
+	_on_main_menu_requested_unpause()
+	# Unhide the "Resume" button on the main menu, so the player can resume
+	# the game after pausing it.
+	resume_button.show()
+
+
+# The player has requested to unpause the game.
+func _on_main_menu_requested_unpause() -> void:
+	# Hide the main menu first.
+	main_menu.hide()
+	# Unpause the game.
 	game.unpause()
+
 
 
 # The game has paused or unpaused.
