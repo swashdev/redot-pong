8eb825cb7ea4c30af525a92ac651048c48229eda
swashberry <swashdev@pm.me>
2024-10-13 04:29:37 -0500

    Add a function to display victory messages.

--- main/main.gd
+++ main/main.gd
@@ -7,8 +7,16 @@ extends Node2D
 @onready var main_menu = $MainMenu
 @onready var resume_button = $MainMenu/Buttons/ResumeButton
 @onready var game = $Game
+@onready var message_box = $MessagePopup
 
 #endregion Child Nodes
+
+#region Game Data
+
+# Stores whether or not the current game is two-player.
+var two_player_game: bool = false
+
+#endregion
 
 
 # Initialization
@@ -18,6 +26,7 @@ func _ready() -> void:
 
 # The "Start Game" button has been pressed, and it is time to start a new game.
 func _on_main_menu_requested_new_game(two_players: bool) -> void:
+	two_player_game = two_players
 	# Instruct `game` to set up for a new game.
 	await game.new_game(two_players)
 	# Unpause the game.
@@ -41,6 +50,19 @@ func _on_game_pause_state_changed(paused: bool) -> void:
 	# If the game has paused, show the menu.
 	if paused:
 		main_menu.show()
+
+
+# A game over has occurred and it is time to declare a winner.
+func _on_game_over(victor: int) -> void:
+	# Hide the "resume" button.  We don't need it anymore.
+	resume_button.hide()
+	if two_player_game:
+		message_box.set_text("Player %d\nWins!" % victor)
+	elif victor == 1:
+		message_box.set_text("You're Winner!")
+	else:
+		message_box.set_text("You're Loser!")
+	message_box.popup_centered()
 
 
 # Alas, the player has pressed the "Quit Game" button :-(
